868945abc896bc2b5e4ffd033fefde0d035e08e6
Fharena <99yoonjoo@gmail.com>
2025-03-17 17:26:55 +0900

    [Silver V] Title: 종이자르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,38 +1,42 @@
-w, h = map(int, input().split())  # 종이 크기 입력
-cut = int(input())  # 자르는 횟수 입력
-
-cutx = []  # 세로로 자르는 위치
-cuty = []  # 가로로 자르는 위치
-x = []  # 세로 조각 크기 저장 리스트
-y = []  # 가로 조각 크기 저장 리스트
+w,h=map(int,input().split())
+cut=int(input())
+cutpaper = []
+cutx=[] ##앞 숫자가 1로 받음
+cuty=[]## 앞숫자가 0으로 받아 가로커팅해야 세로 길이 나옴
+x=[]
+y=[]
 
 for i in range(cut):
-    a, b = map(int, input().split())
-    if a == 1:
+    a,b=map(int,input().split())
+    if a==1:
         cutx.append(b)
     else:
         cuty.append(b)
-
-# 정렬
+##xcut,ycut에 자른 위치 다 넣음
 cutx.sort()
 cuty.sort()
-
-# ✅ 빈 리스트 예외 처리
-if cutx:
-    x.append(cutx[0])  # 첫 번째 자른 부분 크기 추가
+# print(cutx)
+# print(cuty)
+if cutx:  #빈 리스트면 뒤에가서 전체 길이 줄거임
+    x.append(cutx[0])
 if cuty:
     y.append(cuty[0])
 
-# ✅ 자른 부분 간 차이를 리스트에 추가
-for i in range(len(cutx) - 1):
-    x.append(cutx[i + 1] - cutx[i])
-for i in range(len(cuty) - 1):
-    y.append(cuty[i + 1] - cuty[i])
+##차이 집어넣기 x,y에
 
-# ✅ 마지막 조각 크기 추가 (리스트가 비어 있을 경우 대비)
-x.append(w - cutx[-1] if cutx else w)
-y.append(h - cuty[-1] if cuty else h)
+for i in range(len(cutx)-1):
+    x.append(cutx[i+1]-cutx[i])
+x.append(w - cutx[-1] if cutx else w) #안자를때는 w 통째로 주기
 
-# 모든 조각의 넓이를 구한 후 최댓값 찾기
-multlist = [xi * yi for xi in x for yi in y]
+for i in range(len(cuty)-1):
+    y.append(cuty[i+1]-cuty[i])
+y.append(h - cuty[-1] if cuty else h) #안자를때는 h 통째로 주기
+# print(x)
+# print(y)
+
+multlist=[]
+
+for i in range(len(x)):
+    for j in range(len(y)):
+        multlist.append(x[i]*y[j])
 print(max(multlist))

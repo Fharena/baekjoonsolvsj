9d165f5293e51733253e66eccaedd880beae6f74
Fharena <99yoonjoo@gmail.com>
2025-03-31 13:59:59 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 156 ms, Memory: 111992 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+num = list(map(int, input().split()))
+
+operator = list(map(int, input().split()))  # +, -, *, //
+
+maximum = -1e9
+minimum = 1e9
+
+
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+    #0보다 크면 하나씩 줄여가며 체크
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+
+
+dfs(1, num[0], operator[0], operator[1], operator[2], operator[3])
+print(maximum)
+print(minimum)

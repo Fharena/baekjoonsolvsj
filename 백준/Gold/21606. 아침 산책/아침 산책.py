cdf15041f941807e49fb18aebd3f618f03a6e117
Fharena <99yoonjoo@gmail.com>
2025-03-31 11:42:53 +0900

    [Gold III] Title: 아침 산책, Time: 664 ms, Memory: 524172 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,33 +1,44 @@
 import sys
+sys.setrecursionlimit(200000)
+input = sys.stdin.readline
 
-sys.setrecursionlimit(10 ** 5)
-N = int(sys.stdin.readline())  # 정점수
-A = [0] + list(map(int, sys.stdin.readline().strip()))  # 실외 실내 정보를 노드 인덱스 1과 맞추기 위해 [0] 추가
-tree = [[] for _ in range(N + 1)]  # 빈공간 생성
-visit = [0] * (N + 1)  # 방문 체크
+n = int(input())
+
+A = input().rstrip()
+
+graph = [[] for _ in range(n+1)]
+place = [0] * (n+1)
+visited = [0] * (n+1)
+
+for i in range(len(A)):
+    if A[i] == '1':
+        place[i+1] = 1
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def dfs(node):
+    res = 0 
+    for next_node in graph[node]:
+        if place[next_node] == 0:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                res += dfs(next_node)
+        else:
+            res += 1
+    return res
+
 ans = 0
-res = 0
-for _ in range(N - 1):
-    a, b = map(int, sys.stdin.readline().split())
-    tree[a].append(b)
-    tree[b].append(a)
-    if A[a] == 1 and A[b] == 1:  # 둘다 실내일때 경우
-        ans += 2  # 서로 방문하는 케이스가 2개
-
-
-def dfs(v, cnt):  # cnt 실외와 연결된 실내 노드 개수
-    visit[v] = 1
-    for i in tree[v]:
-        if A[i] == 0 and visit[i] == 0:  # 내 외 내 내 외 일경우 마지막 외가 카운팅 안되는 반례
-            cnt = dfs(i, cnt)  # 해당 실외 기점으로 재귀
-        elif A[i] == 1 and visit[i] == 0:  # 실내면서 방문하지않았다면
-            cnt += 1
-    return cnt
-
-
-for i in range(1, N + 1):
-    if visit[i] == 0 and A[i] == 0:  # 방문안한 실외를 기준으로
-        x = dfs(i, 0)  # dfs시작 cnt는 시작이 0
-        res += x * (x - 1)  # 실외를 기준으로 인접한 실내 점 n개가 있을때 경로는 n(n-1)개이다
-
-print(res + ans)
+for i in range(1, n+1):
+    if place[i] == 0:
+        if not visited[i]:
+            visited[i] = 1
+            res = dfs(i)
+            ans += res * (res - 1)
+    else:
+        for next_node in graph[i]:
+            if place[next_node] == 1:
+                ans += 1
+print(ans)

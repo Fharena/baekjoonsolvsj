58cca493d1d96d2745765bdf5b0f39559cdfad3a
Fharena <99yoonjoo@gmail.com>
2025-03-31 20:34:00 +0900

    Update 아침 산책.py

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,44 +1,47 @@
 import sys
-sys.setrecursionlimit(200000)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
 
-n = int(input())
+N=int(input())
+insideout=list(input().strip())
+inout=[0]
+for i in insideout:
+    inout.append(int(i))
 
-A = input().rstrip()
 
-graph = [[] for _ in range(n+1)]
-place = [0] * (n+1)
-visited = [0] * (n+1)
-
-for i in range(len(A)):
-    if A[i] == '1':
-        place[i+1] = 1
-
-for _ in range(n-1):
+graph = [[] for _ in range(N + 1)]
+visited = [0]*(N+1) 
+for _ in range(N-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    
-def dfs(node):
-    res = 0 
-    for next_node in graph[node]:
-        if place[next_node] == 0:
-            if not visited[next_node]:
-                visited[next_node] = 1
-                res += dfs(next_node)
-        else:
-            res += 1
-    return res
 
-ans = 0
-for i in range(1, n+1):
-    if place[i] == 0:
+def dfs(v):
+    k=0
+    visited[v]=1
+
+    for i in graph[v]:
+        if visited[i]==0:
+            if inout[i]==0:
+                visited[i]=1
+                k+=dfs(i)
+
+            else:
+                k+=1
+    return k
+
+
+cnt=0
+for i in range(1,N+1):
+    if inout[i]==1:
+        for j in graph[i]:
+            if inout[j]==1:
+                cnt+=1
+                continue
+
+
+    if inout[i]==0:
         if not visited[i]:
-            visited[i] = 1
-            res = dfs(i)
-            ans += res * (res - 1)
-    else:
-        for next_node in graph[i]:
-            if place[next_node] == 1:
-                ans += 1
-print(ans)
+            k=dfs(i)
+            cnt+=(k*(k-1))
+print(cnt)
